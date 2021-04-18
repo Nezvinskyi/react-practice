e83d96506999d2b5255f846b007f012e3b7c6419
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-04-18 21:39:56 +0200

    adds deleteTodo method

--- src/App.js
+++ src/App.js
@@ -8,12 +8,34 @@ class App extends Component {
     todos: todos,
   };
 
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
   render() {
     const { todos } = this.state;
+    const completedTodos = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0,
+    );
+
+    const pendingTodos = todos.reduce(
+      (acc, todo) => (!todo.completed ? acc + 1 : acc),
+      0,
+    );
+
     return (
       <Layout>
         <h1>Todo List</h1>
-        <TodoList todos={todos} />
+        <div>
+          <p>Total amount of todo: {todos.length}</p>
+          <p>Done: {completedTodos}</p>
+          <p>Pending: {pendingTodos} </p>
+        </div>
+
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
       </Layout>
     );
   }
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,15 +2,17 @@ import "./TodoList.scss";
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos, onDeleteTodo }) => (
 	<ul>
 		{todos.map(({id, text}) =>
 			<li
 				key={id}
 				className='TodoList__item'>
 				<p>{text}</p>
-				<IconButton aria-label="delete">
-					<DeleteIcon />
+				<IconButton
+					aria-label="delete"
+					onClick={()=> onDeleteTodo(id)}>
+						<DeleteIcon />
 				</IconButton>
 			</li>)}
 	</ul>
