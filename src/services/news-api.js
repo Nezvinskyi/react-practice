a3d73381f3bd0c4d981452f69a02d3d76191b17d
Dmitry Nezvinskyi <dmytro@Dmytros-MacBook.local>
2021-04-29 17:02:43 +0200

    updates 06-rest-api

--- src/services/news-api.js
+++ src/services/news-api.js
@@ -4,9 +4,11 @@ axios.defaults.headers.common['Authorization'] =
   'Bearer d082869b7f79460ea045ec72443d6f3f';
 
 const fetchArticles = ({ searchQuery = '', currentPage = 1, pageSize = 5 }) => {
-  return axios.get(
-    `https://newsapi.org/v2/everything?q=${searchQuery}&pageSize=${pageSize}&page=${currentPage}`,
-  );
+  return axios
+    .get(
+      `https://newsapi.org/v2/everything?q=${searchQuery}&pageSize=${pageSize}&page=${currentPage}`,
+    )
+    .then(response => response.data.articles);
 };
 
 export default { fetchArticles };
--- src/views/ArticlesView.js
+++ src/views/ArticlesView.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import SearchForm from '../components/SearchForm/SearchForm';
 import newsApi from '../services/news-api';
 
-axios.defaults.headers.common['Authorization'] =
-  'Bearer d082869b7f79460ea045ec72443d6f3f';
+// axios.defaults.headers.common['Authorization'] =
+// 'Bearer d082869b7f79460ea045ec72443d6f3f';
 
 class ArticlesView extends Component {
   state = {
     articles: [],
     currentPage: 1,
     searchQuery: '',
+    isLoading: false,
+    error: null,
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -20,29 +22,40 @@ class ArticlesView extends Component {
   }
 
   onChangeQuery = query => {
-    this.setState({ searchQuery: query, currentPage: 1, articles: [] });
+    this.setState({
+      searchQuery: query,
+      currentPage: 1,
+      articles: [],
+      error: null,
+    });
   };
 
   fetchArticles = () => {
     const { currentPage, searchQuery } = this.state;
-    const options = {
-      currentPage,
-      searchQuery,
-    };
-    newsApi.fetchArticles(options).then(response => {
-      this.setState(prevState => ({
-        articles: [...prevState.articles, ...response.data.articles],
-        currentPage: prevState.currentPage + 1,
-      }));
-    });
+    const options = { currentPage, searchQuery };
+
+    this.setState({ isLoading: true });
+
+    newsApi
+      .fetchArticles(options)
+      .then(articles => {
+        this.setState(prevState => ({
+          articles: [...prevState.articles, ...articles],
+          currentPage: prevState.currentPage + 1,
+        }));
+      })
+      .catch(error => this.setState({ error }))
+      .finally(() => this.setState({ isLoading: false }));
   };
 
   render() {
-    const { articles } = this.state;
+    const { articles, isLoading, error } = this.state;
+    const shouldRenderLoadMoreButton = articles.length > 0 && !isLoading;
     return (
       <div>
         <h1>Articles</h1>
         <SearchForm onSubmit={this.onChangeQuery} />
+
         <ul>
           {articles.map(({ title, url }) => (
             <li key={title}>
@@ -50,7 +63,10 @@ class ArticlesView extends Component {
             </li>
           ))}
         </ul>
-        {articles.length > 0 && (
+        {error && <h2>Opps! Error!</h2>}
+
+        {isLoading && <h2>...loading</h2>}
+        {shouldRenderLoadMoreButton && (
           <button type="button" onClick={this.fetchArticles}>
             More...
           </button>
